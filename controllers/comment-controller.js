a9d830da19e101d701a245e0c3e07d1cc3335b94
loudagnese <louis.dagnese@gmail.com>
2022-09-04 11:40:45 -0700

    add validation to models and routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -5,7 +5,11 @@ const commentController = {
     console.log(body);
     Comment.create(body)
       .then(({ _id }) => {
-        return Pizza.findOneAndUpdate({ _id: params.pizzaId }, { $push: { comments: _id } }, { new: true });
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true, runValidators: true }
+        );
       })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
@@ -17,7 +21,11 @@ const commentController = {
       .catch((err) => res.json(err));
   },
   addReply({ params, body }, res) {
-    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true, runValidators: true }
+    )
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
